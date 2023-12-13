7c00244fee6b1361db292d719559b48e75566f72
aravikapra <aravikaprakusiya@gmail.com>
2023-12-13 15:07:53 +0700

    Upload Frontend

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use("/merchant", checkRoutes);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
