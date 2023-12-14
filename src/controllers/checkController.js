d4e05798891cd01edd3ce2117e74a0e4393321ef
Oktavian Bayu Widodo <oktavianbayuwidodo7@gmail.com>
2023-12-14 09:42:49 +0700

    update
    update

--- src/controllers/checkController.js
+++ src/controllers/checkController.js
@@ -9,31 +9,30 @@ const checkController = {
     if (isValid) {
       console.log(true);
       checkRepository
-      .saveLog(req.path, true)
-      .then(() => {
-        return checkRepository.updateStatusByUrlPath(req.path, true);
-      })
-      .then(() =>
-        res.json({
-          status: true,
-          message: "Data Valid.",
+        .saveLog(req.path, true)
+        .then(() => {
+          return checkRepository.updateStatusByUrlPath(req.path, true);
         })
-      )
-      .catch((error) => res.status(500).json({ error: error.message }));
-  } else {
-    console.log(false);
-    checkRepository
-    .saveLog(req.path, false)
-    .then(() => {
-      return checkRepository.updateStatusByUrlPath(req.path, false);
-    })
-    .then(() =>
-      res.status(400).json({ status: false, error: "Data tidak valid." })
-    )
-    .catch((error) => res.status(500).json({ error: error.message }));
-}
-},
+        .then(() =>
+          res.json({
+            status: true,
+            message: "Data Valid.",
+          })
+        )
+        .catch((error) => res.status(500).json({ error: error.message }));
+    } else {
+      console.log(false);
+      checkRepository
+        .saveLog(req.path, false)
+        .then(() => {
+          return checkRepository.updateStatusByUrlPath(req.path, false);
+        })
+        .then(() =>
+          res.status(400).json({ status: false, error: "Data tidak valid." })
+        )
+        .catch((error) => res.status(500).json({ error: error.message }));
+    }
+  },
 };
-
 
 module.exports = checkController;
