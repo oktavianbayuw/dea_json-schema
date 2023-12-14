c6c99f3de2eac9c303e7aaa80cf833ce0d62e4f6
Oktavian Bayu Widodo <oktavianbayuwidodo7@gmail.com>
2023-12-14 08:30:20 +0700

    update be
    update be

--- src/routes/checkRoutes.js
+++ src/routes/checkRoutes.js
@@ -3,6 +3,8 @@ const checkController = require("../controllers/checkController");
 
 const router = express.Router();
 
-router.post("/validateAndSave", checkController.validateAndSave);
+router.post("/validateAndSave", (req, res) => {
+  checkController.validateAndSave(req, res);
+});
 
 module.exports = router;
