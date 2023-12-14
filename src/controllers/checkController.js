491eaac795046e20e2f0b01db382bcf689b4422e
Oktavian Bayu Widodo <oktavianbayuwidodo7@gmail.com>
2023-12-14 11:46:28 +0700

    update
    update

--- src/controllers/checkController.js
+++ src/controllers/checkController.js
@@ -5,14 +5,10 @@ const checkController = {
   validateAndSave: (req, res) => {
     const data = req.body;
     const isValid = validate(data);
-    console.log(data);
+
     if (isValid) {
-      console.log(true);
       checkRepository
-        .saveLog(req.path, true)
-        .then(() => {
-          return checkRepository.updateStatusByUrlPath(req.path, true);
-        })
+        .updateStatusByUrlPath(req.path, true)
         .then(() =>
           res.json({
             status: true,
@@ -21,12 +17,8 @@ const checkController = {
         )
         .catch((error) => res.status(500).json({ error: error.message }));
     } else {
-      console.log(false);
       checkRepository
-        .saveLog(req.path, false)
-        .then(() => {
-          return checkRepository.updateStatusByUrlPath(req.path, false);
-        })
+        .updateStatusByUrlPath(req.path, false)
         .then(() =>
           res.status(400).json({ status: false, error: "Data tidak valid." })
         )
